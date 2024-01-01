7cac36401a119d1ef79ce535d4a83f74618146d5
ahmed14 <ahmed.amka@fci.suezuni.edu.eg>
2024-01-01 14:29:19 +0200

    first commit

--- p.py
+++ p.py
@@ -1,6 +1,6 @@
 import subprocess
 
-conversion_command = f"python convert.py darknet191.cfg darknet19.weights model_data/darknet19v3.h5"
+conversion_command = f"python convert.py darknet191.cfg darknet19.weights model_data/darknet19.h5"
 subprocess.run(conversion_command, shell=True)
 
 
